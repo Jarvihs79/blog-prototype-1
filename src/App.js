d6acba842b36bfa3ed4ed490c0d92e7779f747b2
Shivraj Patil <97830217+Jarvihs79@users.noreply.github.com>
2023-04-24 01:03:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Details from "./components/Details";
+import LandingPage from "./components/LandingPage";
+import Navbar from "./components/NavBar";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+     
+    </div>
+  );
+}
+
+export default App;
